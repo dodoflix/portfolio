030ec370848153b3d8ffbed1014af8a257b1ea67
Doğukan Metan <dogukanmetan@gmail.com>
2025-12-10 22:52:52 +0300

    chore(ci): simplify Playwright installation and update base URL

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
 
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './src' }),
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   webServer: {
     command: 'pnpm exec nx dev web',
-    url: 'http://localhost:4200',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env['CI'],
     cwd: workspaceRoot,
     timeout: 180000,
